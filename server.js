0d7702d1f693f6656ed50886b80d4dc66b511c74
bcbridges <bcb35b@gmail.com>
2021-09-01 09:55:36 -0600

    Removed index.js - renamed to server.js

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
 const inquirer = require("inquirer");
+const express = require("express");
+const mysql = require("mysql2");
+const dept = require("./models/Department");
+const role = require("./models/Role");
+const empl = require("./models/Employee");
+
+const app = express();
 
 // Import the connection object
 const sequelize = require("./config/connection");
 
-//Inquirer example
-inquirer.prompt([
-  {
-    type: "input",
-    message: questions[0],
-    name: "appTitle",
-  },
-]);
+// turn on connection to db and server
+// force true to drop and recreate table(s) on every sync
+sequelize
+  .sync({ force: true })
+  .then(() => {
+    console.log("Now listening");
+  })
+  .catch((err) => console.log(err));
 
 // Add function to ask view all depts, view all roles, view all employees, add a dept, add a role, add an empl, and update employee role.
-
 // depts - department names & roles
-
 // roles - job title, role id, the dept that role belongs to, the salary for that role
-
 // empls - employee id, first name, last name, job titles, departments, salaries, managers
-
 // add dept - enter department name
-
 // add role - name, salary, and department for that role
-
 // add empl - first name, last name, role, & manager
-
 // add empl role - select empl from list and update their role
 
 // BONUS
